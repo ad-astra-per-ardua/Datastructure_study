3856d3d85f01f0c77d2d1a58cb135a78621b686c
ddangCONG <85064259+ddangCONG@users.noreply.github.com>
2023-10-07 07:35:29 +0900

    Update kmj.py

--- Problemsolving/02_list_set/01_LS_kmj/kmj.py
+++ Problemsolving/02_list_set/01_LS_kmj/kmj.py
@@ -1,7 +1,6 @@
 n,m = map(int,input().split())
 
 list_S = []
-list_M = []
 count = 0
 
 for i in range(n):
@@ -10,12 +9,8 @@ for i in range(n):
     
 for i in range(m):
     input_M = input()
-    list_M.append(input_M)
-    
-#print(list_S, list_M)
-    
-for i in range(m):
-    if list_M[i] in list_S:
+    if input_M in list_S:
         count += 1
 
 print(count)
+    
