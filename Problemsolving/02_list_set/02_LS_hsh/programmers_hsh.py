8f228195c3b4bca6ef8fcff528b5ee6727c884a3
한상희 <tkdgml822@gmail.com>
2023-10-09 21:13:44 +0900

    programmers_hsh.py Repeated

--- Problemsolving/02_list_set/02_LS_hsh/programmers_hsh.py
+++ Problemsolving/02_list_set/02_LS_hsh/programmers_hsh.py
@@ -1,15 +1,22 @@
+# def solution(spell, dic):
+#     for word in dic:
+#         found = False
+#         for char in spell:
+#             if char not in word:
+#                 found = False
+#                 break
+#             found = True
+            
+#         if found:
+#             return 1
+        
+#     return 2
+
+# 재풀이
 def solution(spell, dic):
     for word in dic:
-        found = False
-        for char in spell:
-            if char not in word:
-                found = False
-                break
-            found = True
-            
-        if found:
+        if set(spell).issubset(set(word)): # issubset을 쓰면 spell이 word에 전부 포함되어있는지 확인해 준다.
             return 1
-        
     return 2
 
 spell = ["z", "d", "x"]
